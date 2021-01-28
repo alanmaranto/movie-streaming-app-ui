b29de0015715babf44325423a4405cd921d1a003
alan <alanarostegui@hotmail.com>
2021-01-27 23:16:29 -0600

    install dependencies and strcture project

---
+++ screens/recents.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+const Recents = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Recents</Text>
+    </View>
+  );
+};
+
+export default Recents;
