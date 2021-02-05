eb1d0c7eb98cdcbc870ea383d2ce3981e421e8b5
alan <alanarostegui@hotmail.com>
2021-02-04 23:51:16 -0600

    style: add styles and icons to tab screens

--- App.js
+++ App.js
@@ -6,16 +6,54 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 import Home from "./screens/home";
 import Profile from "./screens/profile";
 import Recents from "./screens/recents";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}></Tab.Screen>
-        <Tab.Screen name="Recents" component={Recents}></Tab.Screen>
-        <Tab.Screen name="Profile" component={Profile}></Tab.Screen>
+      <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#02ad94"
+        inactiveColor="#dedede"
+        style={{ backgroundColor: "#000" }}
+        barStyle={{ backgroundColor: "#0f0f0f", padding: 4 }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={28} />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Recents"
+          component={Recents}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={28} />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="camera-metering-spot"
+                color={color}
+                size={28}
+              />
+            ),
+          }}
+        ></Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   );
